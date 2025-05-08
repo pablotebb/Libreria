63780f11a4a765c688951acb9456cef17e6a060b
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-08 09:45:28 +0100

    Crear ls formularios usando forms.py, conectarlo a una vista y aplicar validaciones del lado del servidor (parte del trabajo)

--- Libreria/critica/views.py
+++ Libreria/critica/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .forms import Formulario_critica
 
 # Create your views here.
 def home(request):
+  Formulario_critica = Formulario_critica()
   return render(request, 'critica/home.html')
 
--- Libreria/libros/views.py
+++ Libreria/libros/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .forms import Formulario_libros
 
 # Create your views here.
 def home(request):
+  formulario_libros = Formulario_libros()
   return render(request, 'libros/home.html')
 
